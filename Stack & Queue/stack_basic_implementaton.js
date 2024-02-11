34ea21d4de691af182b843a68cfe2feb98202cff
Avijit Ghosh <gavijit622@gmail.com>
2024-02-11 22:05:43 +0530

    basic stack implementation done

---
+++ Stack & Queue/stack_basic_implementaton.js
@@ -0,0 +1,42 @@
+class Stack {
+  constructor(size) {
+    this.items = [];
+    this.size = size;
+    this.lastIndex = -1;
+  }
+
+  isFull() {
+    return this.lastIndex === this.size - 1;
+  }
+
+  isEmpty() {
+    return this.lastIndex < 0;
+  }
+
+  push(item) {
+    if (this.isFull()) {
+      return new Error("Stack overflow");
+    }
+    this.lastIndex++;
+    this.items[this.lastIndex] = item;
+    return this.items;
+  }
+
+  pop() {
+    if (this.isEmpty()) {
+      return new Error("Stack underflow");
+    }
+    return this.items[this.lastIndex--];
+  }
+
+  peek() {
+    return this.items[this.lastIndex];
+  }
+}
+
+const stack = new Stack(3);
+
+console.log(stack.push(12));
+console.log(stack.push(11));
+// console.log(stack.push(1));
+console.log(stack.isFull());
